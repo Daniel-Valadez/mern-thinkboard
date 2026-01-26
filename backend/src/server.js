8487fd03c0986c0711b7398f7499fb24610cb6e4
Daniel Valadez <danielvaladez2017@gmail.com>
2026-01-25 21:33:01 -0800

    Added rate limiting warning to UI and finished displaying note cards on page init.

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import { connectDB } from "./config/db.js";
 import notesRoutes from "./routes/notesRoutes.js"; 
 import dotenv from "dotenv"; 
 import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors"; 
 
 dotenv.config(); 
 
@@ -10,6 +11,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;  
 
 //Middleware to access data packet. 
+app.use(cors({
+  origin: ["http://localhost:5173"], 
+})); 
 app.use(express.json()); 
 app.use(rateLimiter); 
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,59 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React, { useEffect } from "react";
+import Navbar from "../components/Navbar";
+import RateLimitedUI from "../components/RateLimitedUI";
+import { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import NoteCard from "../components/NoteCard";
 
 const HomePage = () => {
+  const [isRatedLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/api/notes");
+        setNotes(res.data);
+        setIsRateLimited(false);
+      } catch (error) {
+        //If we are rated limited, we'll want to display the error from <RateLimitedUI />
+        if (error?.response?.status === 429) {
+          setIsRateLimited(true);
+        } else {
+          //throw a generic error.
+          toast.error("Failed to load the notes.");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNotes();
+  }, []);
+
   return (
     <div>
-      <Navbar /> 
-    </div>
-  )
-}
+      <Navbar />
 
-export default HomePage
+      {isRatedLimited && <RateLimitedUI />}
+
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && (
+          <div className="text-center text-primary py-10">Loading notes...</div>
+        )}
+
+        {notes.length > 0 && !isRatedLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map(note => (
+              <NoteCard key={note._id} note={note} /> 
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
